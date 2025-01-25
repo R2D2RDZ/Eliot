037aa7aec8decc4c874c2815a0991b577c94616b
davedt97 <davedt97@hotmail.com>
2025-01-25 17:55:13 -0600

    Playerjump

--- Assets/PlayerJump.cs
+++ Assets/PlayerJump.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerJump : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class PlayerJump : MonoBehaviour
     [SerializeField] private float jumpHeight = 3f;
     [SerializeField] private LayerMask groundLayers;
     [SerializeField] private LayerMask bounceLayers;
+    [SerializeField] TMP_Text console;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private float gravity = Physics.gravity.y;
@@ -15,46 +17,48 @@ public class PlayerJump : MonoBehaviour
 
     private void Update()
     {
+        console.text = transform.position.ToString();
         bool _isGrounded = IsGrounded();
         bool _isBounce = IsBounce();
+        if (jumpButton.action.WasPressedThisFrame())
+        {
+            Debug.Log("Button pressed");
+            //console.text = "A Pressed";
+        }
         if (jumpButton.action.WasPressedThisFrame() && _isGrounded)
         {
+            Debug.Log("entro al if");
+            //console.text = "Salto";
             Jump();
         }
         if (_isBounce)
         {
+            console.text += "\nIsBounce";
             Bounce();
         }
-        else
-        {
-            movement.y += gravity * Time.deltaTime;
-        }
+
+        movement.y += gravity * Time.deltaTime;
+
         cc.Move(movement * Time.deltaTime);
     }
     private bool IsGrounded()
     {
-        return Physics.CheckSphere(transform.position, 0.5f, groundLayers);
+        
+        return Physics.CheckSphere(transform.position, 0.3f, groundLayers);
     }
 
     private bool IsBounce()
     {
-        return Physics.CheckSphere(transform.position, 0.5f, bounceLayers);
+        
+        return Physics.CheckSphere(transform.position, 0.3f, bounceLayers);
     }
 
     private void Jump()
     {
         movement.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity); 
     }
-
     private void Bounce()
     {
-        movement.y = Mathf.Sqrt((jumpHeight * 50f) * -3.0f * gravity);
-    }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(transform.position, 0.3f);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(transform.position, 0.1f);
+        movement.y = Mathf.Sqrt(jumpHeight * -20.0f * gravity);
     }
 }
