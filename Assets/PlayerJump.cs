f16730a2f5563c52990c0a439182a4b6ee029009
davedt97 <davedt97@hotmail.com>
2025-01-25 16:40:31 -0600

    Podemos aputnar de nuevo

--- Assets/PlayerJump.cs
+++ Assets/PlayerJump.cs
@@ -7,6 +7,7 @@ public class PlayerJump : MonoBehaviour
     [SerializeField] private CharacterController cc;
     [SerializeField] private float jumpHeight = 3f;
     [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private LayerMask bounceLayers;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private float gravity = Physics.gravity.y;
@@ -15,27 +16,45 @@ public class PlayerJump : MonoBehaviour
     private void Update()
     {
         bool _isGrounded = IsGrounded();
-        if (jumpButton.action.WasPressedThisFrame())
-        {
-            Debug.Log("Button pressed");
-        }
+        bool _isBounce = IsBounce();
         if (jumpButton.action.WasPressedThisFrame() && _isGrounded)
         {
-            Debug.Log("entro al if");
             Jump();
         }
-
-        movement.y += gravity * Time.deltaTime;
-
+        if (_isBounce)
+        {
+            Bounce();
+        }
+        else
+        {
+            movement.y += gravity * Time.deltaTime;
+        }
         cc.Move(movement * Time.deltaTime);
     }
     private bool IsGrounded()
     {
-        return Physics.CheckSphere(transform.position, 0.3f, groundLayers);
+        return Physics.CheckSphere(transform.position, 0.5f, groundLayers);
+    }
+
+    private bool IsBounce()
+    {
+        return Physics.CheckSphere(transform.position, 0.5f, bounceLayers);
     }
 
     private void Jump()
     {
         movement.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity); 
+    }
+
+    private void Bounce()
+    {
+        movement.y = Mathf.Sqrt((jumpHeight * 50f) * -3.0f * gravity);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 0.3f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 0.1f);
     }
 }
