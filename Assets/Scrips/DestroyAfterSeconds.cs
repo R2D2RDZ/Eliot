63d54f9ef3bbd4ab9fbb145e2c2f3b8a659b71ab
davedt97 <davedt97@hotmail.com>
2025-01-26 14:46:22 -0600

    Pop goes the bubble!

--- Assets/Scrips/DestroyAfterSeconds.cs
+++ Assets/Scrips/DestroyAfterSeconds.cs
@@ -3,10 +3,28 @@ using UnityEngine;
 public class DestroyAfterSeconds : MonoBehaviour
 {
     [SerializeField] float seconds;
+
+    [Header("Clips de música")]
+    public AudioClip popSound; // Música por defecto
+
+    [Header("Configuración del audio")]
+    public AudioSource audioSource; // Componente de AudioSource para reproducir la música
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        PlayMusic(popSound);
         Destroy(gameObject, seconds);
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (audioSource.clip != clip)
+        {
+            audioSource.Stop();
+            audioSource.loop = false; // Activa el loop para este clip
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
     }
 
 }
--- Assets/Scrips/destroyBubble.cs
+++ Assets/Scrips/destroyBubble.cs
@@ -30,6 +30,8 @@ public class destroyBubble : MonoBehaviour
         if (!string.IsNullOrEmpty(sceneToLoad))
         {
             SceneManager.LoadScene(sceneToLoad);
+
         }
     }
+
 }
