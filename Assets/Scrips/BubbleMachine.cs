71409b4df50b24bd732943d88a22c8935c4046da
davedt97 <davedt97@hotmail.com>
2025-01-26 06:41:27 -0600

    Pantalla chida solo faltan algunas cosillas
    Cambiar escena, texto y cambiar color del cielo pq se ve raro

--- Assets/Scrips/BubbleMachine.cs
+++ Assets/Scrips/BubbleMachine.cs
@@ -2,28 +2,42 @@ using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class BubbleMachine
-{
+public class BubbleMachine : MonoBehaviour { 
+
     public GameObject bulletPrefab; // Prefab del proyectil
     public Transform firePoint; // Punto desde donde se dispararán las balas
     public float bulletSpeed = 5f; // Velocidad del proyectil
+
+    private int count = 0;
     void FixedUpdate()
     {
+        float randomY = Random.Range(0f, 360f);
+        float randomX = Random.Range(45f, 135f);
+        transform.rotation = Quaternion.Euler(randomX, randomY, transform.rotation.eulerAngles.z);
         Shoot();
     }
     void Shoot()
     {
-        // Instancia una bala en el FirePoint
-        GameObject bullet = Object.Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-
-        // Aplica velocidad al Rigidbody de la bala
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        if (rb != null)
+        if (count == 5)
         {
-            rb.linearVelocity = firePoint.forward * bulletSpeed;
-        }
+            // Instancia una bala en el FirePoint
+            GameObject bullet = Object.Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-        // Destruye la bala después de 2 segundos
-        Object.Destroy(bullet, 2f);
+            // Aplica velocidad al Rigidbody de la bala
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.linearVelocity = firePoint.forward * bulletSpeed;
+            }
+
+            // Destruye la bala después de 2 segundos
+            Object.Destroy(bullet, 2f);
+            count = 0;
+        }
+        else
+        {
+            count++;
+        }
     }
 }
+
